077fde1d4dcf2f9ce381626a04e4e6671dbf95a8
Noelia Abraham <noeliasabraham@gmail.com>
2022-10-15 19:32:22 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -284,33 +284,13 @@
      };
      OnePageNav();
 
-     var carousel = function() {
-         $('.featured-carousel').owlCarousel({
-             loop: true,
-             autoplay: true,
-             margin: 30,
-             animateOut: 'fadeOut',
-             animateIn: 'fadeIn',
-             nav: true,
-             dots: true,
-             autoplayHoverPause: false,
-             items: 1,
-             navText: ["<p><small>Prev</small><span class='ion-ios-arrow-round-back'></span></p>", "<p><small>Next</small><span class='ion-ios-arrow-round-forward'></span></p>"],
-             responsive: {
-                 0: {
-                     items: 1
-                 },
-                 600: {
-                     items: 1
-                 },
-                 1000: {
-                     items: 1
-                 }
-             }
-         });
+     // parallax
 
-     };
-     carousel();
+
+     var scene = document.getElementById('scene');
+     var parallaxInstance = new Parallax(scene);
+
+
 
 
 
